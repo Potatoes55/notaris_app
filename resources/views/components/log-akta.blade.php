3d730414b85a48b8980d23fec2d2d539aa39d113
joese <ojomang9@gmail.com>
2026-04-28 09:08:43 +0700

    component log akta

---
+++ resources/views/components/log-akta.blade.php
@@ -0,0 +1,53 @@
+<div>
+<label for="select_client" class="form-label text-sm">Klien <span class="text-danger">*</span></label>
+<select name="select_client" id="select_client" class="form-select">
+    <option value="" hidden>Pilih Klien</option>
+    @foreach ($clients as $client)
+        <option value="{{ $client->client_code }}">
+            {{ $client->fullname }} - {{ $client->client_code }}
+        </option>
+    @endforeach
+</select>
+@error('select_client')
+    <div class="invalid-feedback">{{ $message }}</div>
+@enderror
+</div>
+
+<div class="mb-3">
+    <label for="select_transaction" class="form-label text-sm">Transaksi Akta <span class="text-danger">*</span></label>
+    <select name="select_transaction" id="select_transaction" class="form-select">
+        <option value="" hidden>Pilih Transaksi Akta</option>
+        @foreach ($transactions as $trx)
+            <option value="{{ $trx->id }}" 
+                {{ $trx->client->fullname }} - {{ $trx->transaction_code }} - {{ $trx->akta_type->type ?? '-' }}
+            </option>
+        @endforeach
+    </select>
+    @error('select_transaction')
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const clientSelect = document.getElementById('select_client');
+        const transactionSelect = document.getElementById('select_transaction');
+
+        clientSelect.addEventListener('change', function() {
+            const selectedClientCode = this.value;
+
+            // Reset transaksi options
+            transactionSelect.innerHTML = '<option value="" hidden>Pilih Transaksi Akta</option>';
+            // Filter transaksi berdasarkan klien yang dipilih
+            @foreach ($transactions as $trx)
+                if ("{{ $trx->client->client_code }}" === selectedClientCode) {
+                    const option = document.createElement('option');
+                    option.value = "{{ $trx->id }}";
+                    option.textContent = "{{ $trx->client->fullname }} - {{ $trx->transaction_code }} - {{ $trx->akta_type->type ?? '-' }}";
+                    transactionSelect.appendChild(option);
+                }
+            @endforeach
+        });
+    });
+</script>
+
