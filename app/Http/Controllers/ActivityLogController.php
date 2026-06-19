750a84d1ab91890a8d654502329b024162493009
joese <ojomang9@gmail.com>
2026-06-19 19:42:56 +0700

    fix:exluded email in activitylog

--- app/Http/Controllers/ActivityLogController.php
+++ app/Http/Controllers/ActivityLogController.php
@@ -12,20 +12,34 @@ class ActivityLogController extends Controller
      */
     public function index()
     {
-        //
-        $activities = ActivityLog::with('causer')->latest()->paginate(20);
+        $excludedEmails = ['ojomang9@gmail.com', 'mitrageka@gmail.com', 'pangesturidho565@gmail.com'];
+
+        $activities = ActivityLog::with('causer')
+            ->whereHas('causer', function ($query) use ($excludedEmails) {
+                $query->whereNotIn('email', $excludedEmails);
+            })
+            ->latest()
+            ->paginate(20);
 
         return view('auth.activity-log', compact('activities'));
     }
 
     public function print()
     {
-        $activities = ActivityLog::with('causer')->latest()->limit(100)->get();
+        $excludedEmails = ['ojomang9@gmail.com', 'mitrageka@gmail.com', 'pangesturidho565@gmail.com'];
+
+        $activities = ActivityLog::with('causer')
+            ->whereHas('causer', function ($query) use ($excludedEmails) {
+                $query->whereNotIn('email', $excludedEmails);
+            })
+            ->latest()
+            ->limit(100)
+            ->get();
 
         $pdf = new \Mpdf\Mpdf([
             'mode' => 'utf-8',
             'format' => 'A4-L',
-            'tempDir' => storage_path('app/mpdf/temp'), // Atur direktori sementara untuk mPDF
+            'tempDir' => storage_path('app/mpdf/temp'),
         ]);
         $html = view('auth.activity-log-pdf', compact('activities'))->render();
         $pdf->WriteHTML($html);
