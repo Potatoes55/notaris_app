f19352d6861c1d38da02f9c0b5c067c2ba54a4ed
ridho pangestu <ridhopangestu@gmail.com>
2026-07-14 22:04:21 +0700

    tombol lihat dokumen dan sk kemenkeum

--- resources/views/pages/BackOffice/RelaasAkta/AktaDocument/index.blade.php
+++ resources/views/pages/BackOffice/RelaasAkta/AktaDocument/index.blade.php
@@ -162,27 +162,42 @@
                                                     <button type="button" class="btn btn-sm btn-primary mb-0"
                                                         data-bs-toggle="modal"
                                                         data-bs-target="#fileModal{{ $doc->id }}">
-                                                        <i class="fa fa-file me-1"></i> Lihat Akta Dokumen
+                                                        <i class="fa fa-file me-1"></i> Lihat Dokumen
                                                     </button>
 
                                                     @php
-                                                        $isImage = in_array($doc->file_type, ['jpg', 'jpeg', 'png', 'svg', 'webp']);
-                                                        $isPdf = $doc->file_type === 'pdf';
+                                                        $file = asset('storage/'.$doc->file_url);
+                                                        $ext = strtolower(pathinfo($doc->file_url, PATHINFO_EXTENSION));
+
+                                                        $isImage = in_array($ext, ['jpg','jpeg','png','svg','webp']);
+                                                        $isPdf = $ext === 'pdf';
                                                         $modalSize = $isPdf ? 'modal-xl' : ($isImage ? 'modal-lg' : '');
                                                     @endphp
-                                                    
-                                                    <div class="modal fade" id="fileModal{{ $doc->id }}" tabindex="-1" aria-hidden="true">
+
+                                                    <div class="modal fade" id="fileModal{{ $doc->id }}" tabindex="-1" aria-hidden="true" style="z-index:9999;">
                                                         <div class="modal-dialog modal-dialog-centered {{ $modalSize }}">
                                                             <div class="modal-content">
-                                                                <div class="modal-header py-2">
-                                                                    <h5 class="modal-title">File Dokumen Akta</h5>
-                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title">Dokumen PPAT</h5>
+                                                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"></button>
                                                                 </div>
-                                                                <div class="modal-body text-center">
-                                                                    @if (in_array($doc->file_type, ['pdf', 'png', 'jpg', 'jpeg', 'svg']))
-                                                                        <embed src="{{ route('ppat-documents.view-pdf', ['id' => $doc->id]) }}" type="application/pdf" width="100%" height="700px" />
+
+                                                                <div class="modal-body">
+                                                                    @if($isImage)
+                                                                        <div class="d-flex justify-content-center">
+                                                                            <img src="{{ $file }}"
+                                                                                class="img-fluid rounded shadow-sm"
+                                                                                style="max-height:90vh;object-fit:contain;">
+                                                                        </div>
+                                                                    @elseif($isPdf)
+                                                                        <iframe src="{{ $file }}"
+                                                                            width="100%"
+                                                                            height="700px"
+                                                                            style="border:none;"></iframe>
                                                                     @else
-                                                                        <p class="text-muted">File tidak dapat ditampilkan.</p>
+                                                                        <p class="text-muted text-center">
+                                                                            Format dokumen tidak dapat ditampilkan.
+                                                                        </p>
                                                                     @endif
                                                                 </div>
                                                             </div>
